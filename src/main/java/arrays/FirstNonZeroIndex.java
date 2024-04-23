7d04efd359daa545f98235760f59b1080ff8fff1
ElliotLuque <elliotluque@gmail.com>
2024-04-23 10:56:58 +0200

    add docstring

--- src/main/java/arrays/FirstNonZeroIndex.java
+++ src/main/java/arrays/FirstNonZeroIndex.java
@@ -1,6 +1,12 @@
 package arrays;
 
 public class FirstNonZeroIndex {
+    /**
+     * Returns the index of the first non-zero integer in the array
+     * @param array The array of integers
+     * @param pos The initial pos for searching
+     * @return The index of the first non-zero integer
+     */
     public static int firstNonZeroIndex(int[] array, int pos) {
         if (pos == array.length) { return -1; }
         else if (array[pos] != 0) { return pos; }
